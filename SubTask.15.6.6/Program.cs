bc53ad8eb74b210cdef2a3be67daaba0745d75f7
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-03-18 18:59:27 +0300

    Добавьте файлы проекта.

---
+++ SubTask.15.6.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using SubTask._15._6._6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Task1
+{
+    class Program : SubTask._15._6._6.Classroom
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = classes.SelectMany(c => c.Students).ToArray();
+            return allStudents;
+        }
+    }
+}
